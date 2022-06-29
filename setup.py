cf0d9d017a6bafbf9d9da4fc01519656a1d026b2
Ayush Tambde <tambdeayush@gmail.com>
2022-06-29 14:00:58 +0100

    fix

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='hodl_qiskit',
-    version='1.0.3',    
+    version='1.0.2',    
     description='A Qiskit Python package to call HODL inline',
     url='https://github.com/at2005/hodl-qiskit',
     author='Ayush Tambde',
     author_email='tambdeayush@gmail.com',
     license='MIT',
     packages=find_packages(),
-
+package_dir={'':'src'},
     classifiers=[
         'Development Status :: Production',
         'Intended Audience :: Science/Research',
