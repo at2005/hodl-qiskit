75ae1e767edc6426de7cc49bd65fe3ae5a2f1f7c
Ayush Tambde <tambdeayush@gmail.com>
2022-06-29 14:33:59 +0100

    okay okay

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from distutils.core import setup, find_packages
 
 setup(
     name='hodl_qiskit',
@@ -8,6 +8,7 @@ setup(
     author='Ayush Tambde',
     author_email='tambdeayush@gmail.com',
     license='MIT',
+    package_dir={'':'src'},
     packages=find_packages(),
     classifiers=[
         'Development Status :: Production',
